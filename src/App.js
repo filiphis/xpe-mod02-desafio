825cfa82f96eb6b66b69cbad727658561af66c4c
Luiz Felipe da Silveira <l.filiphis@hotmail.com>
2023-03-21 23:25:53 -0300

    Desenvolvendo o componente Card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import { Card } from "./components/Card";
+
 export default function App() {
-  console.log('Teste no console do navegador')
+  console.log("Teste no console do navegador");
 
   return (
-    <div>
+    <>
       <header>
-        <div className="bg-gray-100 mx-auto p-4">
-          <h1 className="text-center font-semibold text-xl">
-            Projeto base para o Módulo React I
+        <div className="bg-blue-500 mx-auto p-4">
+          <h1 className="text-center font-semibold text-xl text-white">
+            React Elections
           </h1>
         </div>
       </header>
@@ -14,8 +16,9 @@ export default function App() {
       <main>
         <div className="container mx-auto p-4">
           <h2>O conteúdo fica aqui.</h2>
+          <Card />
         </div>
       </main>
-    </div>
-  )
+    </>
+  );
 }
